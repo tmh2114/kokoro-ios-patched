592173dd56edae1778a9fda5bc7316ccb238779c
Tyler <tyler.m.hendry@gmail.com>
2026-03-22 19:59:07 -0400

    fork: patched kokoro-ios for Otaku5 (MLXFast dep, try! fixes, static lib)

---
+++ Package.swift
@@ -0,0 +1,46 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "KokoroSwift",
+  platforms: [
+    .iOS(.v18), .macOS(.v15)
+  ],
+  products: [
+    .library(
+      name: "KokoroSwift",
+      type: .static,
+      targets: ["KokoroSwift"]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.29.1"),
+    // .package(url: "https://github.com/mlalma/eSpeakNGSwift", from: "1.0.1"),
+    .package(url: "https://github.com/mlalma/MisakiSwift", from: "1.0.4"),
+    .package(url: "https://github.com/mlalma/MLXUtilsLibrary.git", from: "0.0.6")
+  ],
+  targets: [
+    .target(
+      name: "KokoroSwift",
+      dependencies: [
+        .product(name: "MLX", package: "mlx-swift"),
+        .product(name: "MLXNN", package: "mlx-swift"),
+        .product(name: "MLXRandom", package: "mlx-swift"),
+        .product(name: "MLXFFT", package: "mlx-swift"),
+        .product(name: "MLXFast", package: "mlx-swift"),
+        // .product(name: "eSpeakNGLib", package: "eSpeakNGSwift"),
+        .product(name: "MisakiSwift", package: "MisakiSwift"),
+        .product(name: "MLXUtilsLibrary", package: "MLXUtilsLibrary")
+      ],
+      resources: [
+       .copy("Resources/")
+      ]
+    ),
+    .testTarget(
+      name: "KokoroSwiftTests",
+      dependencies: ["KokoroSwift"]
+    ),
+  ]
+)
