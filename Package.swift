5ef9bd54a1c523e5265e15dbf56de1deb978d0e6
Tyler <tyler.m.hendry@gmail.com>
2026-03-23 00:16:42 -0400

    fix: update mlx-swift to 0.30.6 (fixes Device::Device crash)

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.29.1"),
+    .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.30.6"),
     // .package(url: "https://github.com/mlalma/eSpeakNGSwift", from: "1.0.1"),
     .package(url: "https://github.com/mlalma/MisakiSwift", from: "1.0.4"),
     .package(url: "https://github.com/mlalma/MLXUtilsLibrary.git", from: "0.0.7")
